5ec8e2d717e77b5232497eb9a3d2dba35f173527
Moh. Saidul Musthofa <sayyidmusthofa89@gmail.com>
2020-04-22 13:46:41 +0700

    lupa pass

---
+++ NurseryPolije/app/src/main/java/com/example/nurserypolije/ui/LupaPassword.java
@@ -0,0 +1,16 @@
+package com.example.nurserypolije.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nurserypolije.R;
+
+public class LupaPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lupa_password);
+    }
+}
