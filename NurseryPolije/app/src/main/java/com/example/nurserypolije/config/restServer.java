9585393a91aff88678f320678167c78228ae526f
Moh. Saidul Musthofa <sayyidmusthofa89@gmail.com>
2020-05-14 11:42:51 +0700

    peta lokasi

--- NurseryPolije/app/src/main/java/com/example/nurserypolije/config/restServer.java
+++ NurseryPolije/app/src/main/java/com/example/nurserypolije/config/restServer.java
@@ -2,11 +2,15 @@ package com.example.nurserypolije.config;
 
 public class restServer {
     //URL foto bunga
-    public static final String URL_FOTO_BUNGA = "http://192.168.43.243/nuporyV2/Justify/assets/img/fotobunga/";
+    //public static final String URL_FOTO_BUNGA = "http://192.168.43.243/nuporyV2/Justify/assets/img/fotobunga/";
+    public static final String URL_FOTO_BUNGA = "http://192.168.18.18/nuporyV2/Justify/assets/img/fotobunga/"; //ip sayyid
     //URL login
-    public static final String URL_LOGIN = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Auth";
+    //public static final String URL_LOGIN = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Auth";
+    public static final String URL_LOGIN = "http://192.168.18.18/nuporyV2/Justify/rest_ci/Auth"; //ip sayyid
     //URL Profile
-    public static final String URL_PROFILE = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Profile";
+   // public static final String URL_PROFILE = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Profile";
+    public static final String URL_PROFILE = "http://192.168.18.18/nuporyV2/Justify/rest_ci/Profile"; //ip sayyid
     //URL Beranda
-    public static final String URL_BERANDA = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Beranda";
+    //public static final String URL_BERANDA = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Beranda";
+    public static final String URL_BERANDA = "http://192.168.18.18/nuporyV2/Justify/rest_ci/Beranda";
 }
