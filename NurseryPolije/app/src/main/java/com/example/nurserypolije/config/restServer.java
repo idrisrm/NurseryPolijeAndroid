461743934697af7d905b4f4201e4e67938a22cb2
idris <hendrytifaa@gmail.com>
2020-05-25 08:35:42 +0700

    Update restServer.java

--- NurseryPolije/app/src/main/java/com/example/nurserypolije/config/restServer.java
+++ NurseryPolije/app/src/main/java/com/example/nurserypolije/config/restServer.java
@@ -2,8 +2,8 @@ package com.example.nurserypolije.config;
 
 public class restServer {
     //URL foto bunga
-    //public static final String URL_FOTO_BUNGA = "http://192.168.43.243/nuporyV2/Justify/assets/img/fotobunga/";
-    public static final String URL_FOTO_BUNGA = "http://192.168.43.55/nuporyV2/Justify/assets/img/fotobunga/"; //ip sayyid
+    public static final String URL_FOTO_BUNGA = "http://192.168.43.243/nuporyV2/Justify/assets/img/fotobunga/";
+//    public static final String URL_FOTO_BUNGA = "http://192.168.43.55/nuporyV2/Justify/assets/img/fotobunga/"; //ip sayyid
     //public static final String URL_FOTO_BUNGA = "http://192.168.43.11/nuporyV2/Justify/assets/img/fotobunga/"; //ip Ridho
 
     //URL login
@@ -12,24 +12,24 @@ public class restServer {
     //public static final String URL_LOGIN = "http://192.168.43.11/nuporyV2/Justify/rest_ci/Auth"; //ip Ridho
 
     //URL Profile
-    //public static final String URL_PROFILE = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Profile";
-    public static final String URL_PROFILE = "http://192.168.43.55/nuporyV2/Justify/rest_ci/Profile"; //ip sayyid
+    public static final String URL_PROFILE = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Profile";
+//    public static final String URL_PROFILE = "http://192.168.43.55/nuporyV2/Justify/rest_ci/Profile"; //ip sayyid
    //public static final String URL_PROFILE = "http://192.168.43.11/nuporyV2/Justify/rest_ci/Profile"; //ip Ridho
 
     //URL Beranda
-    //public static final String URL_BERANDA = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Beranda";
-    public static final String URL_BERANDA = "http://192.168.43.55/nuporyV2/Justify/rest_ci/Beranda"; //ip sayyid
+    public static final String URL_BERANDA = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Beranda";
+//    public static final String URL_BERANDA = "http://192.168.43.55/nuporyV2/Justify/rest_ci/Beranda"; //ip sayyid
     // public static final String URL_BERANDA = "http://192.168.43.11/nuporyV2/Justify/rest_ci/Beranda"; //ip Ridho
 
     //URL Keranjang
-    //public static final String URL_KERANJANG = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Transaksi/Keranjang";
-    public static final String URL_KERANJANG = "http://192.168.43.55/nuporyV2/Justify/rest_ci/Transaksi/Keranjang"; //ip sayyid
+    public static final String URL_KERANJANG = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Transaksi/Keranjang";
+//    public static final String URL_KERANJANG = "http://192.168.43.55/nuporyV2/Justify/rest_ci/Transaksi/Keranjang"; //ip sayyid
 
     //URL ubahProfil
     //public static final String URL_UBAH_PROFILE = "http://192.168.43.11/nuporyV2/Justify/rest_ci/Profile/ubahProfil"; //ip Ridho
     public static final String URL_UBAH_PROFILE = "http://192.168.43.55/nuporyV2/Justify/rest_ci/Profile/ubahProfil"; //ip Sayyid
 
     //URL Keranjang User
-    //public static final String URL_KERANJANG_USER = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Transaksi/KeranjangUser";
-    public static final String URL_KERANJANG_USER = "http://192.168.43.55/nuporyV2/Justify/rest_ci/Transaksi/KeranjangUser"; // ip sayyid
+    public static final String URL_KERANJANG_USER = "http://192.168.43.243/nuporyV2/Justify/rest_ci/Transaksi/KeranjangUser";
+//    public static final String URL_KERANJANG_USER = "http://192.168.43.55/nuporyV2/Justify/rest_ci/Transaksi/KeranjangUser"; // ip sayyid
 }
